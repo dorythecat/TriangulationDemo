4dcbfa9a03d0fc9df2338ebbea21339cce42f12e
dorythecat <pemegomachado@gmail.com>
2025-09-20 23:53:41 +0200

    Added JS code to make circles draggable Easily extends to ALL circle class objects in scene, pretty neat if I say so myself :3

---
+++ res/script.js
@@ -0,0 +1,28 @@
+const circles = document.querySelectorAll('.circle');
+
+let selectedCircle = null;
+let offsetX, offsetY;
+
+function startDrag(e) {
+    selectedCircle = e.target;
+    offsetX = e.clientX - selectedCircle.getBoundingClientRect().left;
+    offsetY = e.clientY - selectedCircle.getBoundingClientRect().top;
+    document.addEventListener('mousemove', drag);
+    document.addEventListener('mouseup', endDrag);
+}
+
+function drag(e) {
+    if (!selectedCircle) return;
+    selectedCircle.style.left = `${e.clientX - offsetX}px`;
+    selectedCircle.style.top = `${e.clientY - offsetY}px`;
+}
+
+function endDrag() {
+    document.removeEventListener('mousemove', drag);
+    document.removeEventListener('mouseup', endDrag);
+    selectedCircle = null;
+}
+
+circles.forEach(circle => {
+    circle.addEventListener('mousedown', startDrag);
+});
